a10ffbeeb81abca33798cd051d83f6892b5d944a
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2018-07-11 10:59:56 +0300

    Also generate roster files for Prosody

--- c_src/rtb_db.c
+++ c_src/rtb_db.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <sys/stat.h>
 
+#define BUFSIZE 65535
 #define USERS_CSV_FILE "/tmp/users.csv"
 #define ROSTERS_CSV_FILE "/tmp/rosters.csv"
 #define USERS_DIR "/tmp/accounts/"
@@ -16,60 +17,58 @@ char *timestamp() {
   return "1970-01-01 00:00:00";
 }
 
-char *replace(char *str) {
-  ssize_t len = strlen(str);
-  char *buf = calloc(1, 1000);
-  if (buf) {
-    int i = 0, j = 0;
-    while (i<len) {
-      if (str[i] == '%') {
-	buf[j++] = '%';
-	buf[j++] = 'l';
-	buf[j++] = 'u';
-      } else {
-	buf[j++] = str[i];
-      }
-      i++;
+void replace(char *dst, const char *src, const char c, const char *str) {
+  ssize_t len = strlen(src);
+  memset(dst, 0, BUFSIZE);
+  int i = 0, j = 0;
+  while (i<len) {
+    if (src[i] == c) {
+      strcpy(dst+j, str);
+      j += strlen(str);
+    } else {
+      dst[j++] = src[i];
     }
+    i++;
   }
-  return buf;
 }
 
-char *mk_user_csv_row(char *argv[]) {
+void mk_user_csv_row(char *row, char *argv[]) {
   char *user = argv[4];
   char *password = argv[6];
-  char *buf = calloc(1, 1000);
-  char *res = NULL;
-  if (buf) {
-    sprintf(buf, "\"%s\",\"%s\",\"\",\"\",\"0\",\"%s\"\n", user, password, timestamp());
-    res = replace(buf);
-  }
-  return res;
+  char buf[BUFSIZE];
+  memset(buf, 0, BUFSIZE);
+  sprintf(buf, "\"%s\",\"%s\",\"\",\"\",\"0\",\"%s\"\n", user, password, timestamp());
+  replace(row, buf, '%', "%lu");
 }
 
-char *mk_roster_csv_row(char *argv[]) {
+void mk_roster_csv_row(char *row, char *argv[]) {
   char *user = argv[4];
   char *server = argv[5];
-  char *buf = calloc(1, 1000);
-  char *res = NULL;
-  if (buf) {
-    sprintf(buf,
-	    "\"%s\",\"%s@%s\",\"%s\",\"B\",\"N\",\"\",\"N\",\"\",\"item\",\"%s\"\n",
-	    user, user, server, user, timestamp());
-    res = replace(buf);
-  }
-  return res;
+  char buf[BUFSIZE];
+  sprintf(buf,
+	  "\"%s\",\"%s@%s\",\"%s\",\"B\",\"N\",\"\",\"N\",\"\",\"item\",\"%s\"\n",
+	  user, user, server, user, timestamp());
+  replace(row, buf, '%', "%lu");
 }
 
-char *mk_user_dat(char *argv[]) {
+void mk_user_dat(char *data, char *argv[]) {
   char *password = argv[6];
-  char *buf = calloc(1, 1000);
-  char *res = NULL;
-  if (buf) {
-    sprintf(buf, "return {\n\t[\"password\"] = \"%s\";\n};\n", password);
-    res = replace(buf);
-  }
-  return res;
+  char buf[BUFSIZE];
+  sprintf(buf, "return {\n\t[\"password\"] = \"%s\";\n};\n", password);
+  replace(data, buf, '%', "%lu");
+}
+
+void mk_roster_dat(char *data, char *argv[]) {
+  char *user = argv[4];
+  char *server = argv[5];
+  char buf[BUFSIZE];
+  sprintf(buf, "\t[\"%s@%s\"] = {\n"
+	  "\t\t[\"subscription\"] = \"both\";\n"
+	  "\t\t[\"groups\"] = {};\n"
+	  "\t\t[\"name\"] = \"%s\";\n"
+	  "\t};\n",
+	  user, server, user);
+  replace(data, buf, '%', "%lu");
 }
 
 int generate_users_csv(long int capacity, char *argv[]) {
@@ -81,20 +80,16 @@ int generate_users_csv(long int capacity, char *argv[]) {
 
   printf("Generating %s... ", USERS_CSV_FILE);
   fflush(stdout);
-  char *pattern = mk_user_csv_row(argv);
-  if (pattern) {
-    for (long int i=1; i<=capacity; i++) {
-      if (fprintf(fd, pattern, i, i) < 0) {
-	printf("Failed to write to file %s: %s\n", USERS_CSV_FILE, strerror(errno));
-	return errno;
-      }
+  char row[BUFSIZE];
+  mk_user_csv_row(row, argv);
+  for (long int i=1; i<=capacity; i++) {
+    if (fprintf(fd, row, i, i) < 0) {
+      printf("Failed to write to file %s: %s\n", USERS_CSV_FILE, strerror(errno));
+      return errno;
     }
-    printf("done\n");
-    return 0;
-  } else {
-    printf("Failed to generate CSV row for %s: %s\n", USERS_CSV_FILE, strerror(ENOMEM));
-    return ENOMEM;
   }
+  printf("done\n");
+  return 0;
 }
 
 int generate_rosters_csv(long int capacity, char *argv[]) {
@@ -107,26 +102,22 @@ int generate_rosters_csv(long int capacity, char *argv[]) {
   printf("Generating %s... ", ROSTERS_CSV_FILE);
   fflush(stdout);
   long int i, j, next, prev;
-  char *pattern = mk_roster_csv_row(argv);
-  if (pattern) {
-    for (i=1; i<=capacity; i++) {
-      for (j=1; j<=10; j++) {
-	next = i + j;
-	next = (next > capacity) ? (next - capacity): next;
-	prev = (i <= j) ? capacity - (j - 1) : i-j;
-	if ((fprintf(fd, pattern, i, next, next) < 0) ||
-	    (fprintf(fd, pattern, i, prev, prev) < 0)) {
-	  printf("Failed to write to file %s: %s\n", ROSTERS_CSV_FILE, strerror(errno));
-	  return errno;
-	}
+  char row[BUFSIZE];
+  mk_roster_csv_row(row, argv);
+  for (i=1; i<=capacity; i++) {
+    for (j=1; j<=10; j++) {
+      next = i + j;
+      next = (next > capacity) ? (next % capacity): next;
+      prev = (i <= j) ? capacity - (j - 1) : i-j;
+      if ((fprintf(fd, row, i, next, next) < 0) ||
+	  (fprintf(fd, row, i, prev, prev) < 0)) {
+	printf("Failed to write to file %s: %s\n", ROSTERS_CSV_FILE, strerror(errno));
+	return errno;
       }
     }
-    printf("done\n");
-    return 0;
-  } else {
-    printf("Failed to generate CSV row for %s: %s\n", USERS_CSV_FILE, strerror(ENOMEM));
-    return ENOMEM;
   }
+  printf("done\n");
+  return 0;
 }
 
 int generate_user_files(long int capacity, char *argv[]) {
@@ -142,34 +133,83 @@ int generate_user_files(long int capacity, char *argv[]) {
   long int i;
   FILE *fd;
   ssize_t dir_len = strlen(USERS_DIR);
-  char *password = mk_user_dat(argv);
-  char *user = replace(argv[4]);
-  char *file = calloc(1, 1000);
+  char password[BUFSIZE];
+  char user[BUFSIZE];
+  char file[BUFSIZE];
+  mk_user_dat(password, argv);
+  replace(user, argv[4], '%', "%lu");
+  memset(file, 0, BUFSIZE);
   strcpy(user+strlen(user), ".dat");
   strcpy(file, USERS_DIR);
-  if (password && user && file) {
-    for (i=1; i<=capacity; i++) {
-      sprintf(file+dir_len, user, i);
-      fd = fopen(file, "w");
-      if (!fd) {
-	printf("Failed to open file %s for writing: %s\n", file, strerror(errno));
-	return errno;
-      }
-      if (fprintf(fd, password, i) < 0) {
+  for (i=1; i<=capacity; i++) {
+    sprintf(file+dir_len, user, i);
+    fd = fopen(file, "w");
+    if (!fd) {
+      printf("Failed to open file %s for writing: %s\n", file, strerror(errno));
+      return errno;
+    }
+    if (fprintf(fd, password, i) < 0) {
+      printf("Failed to write to file %s: %s\n", file, strerror(errno));
+      return errno;
+    }
+    fclose(fd);
+  }
+  printf("done\n");
+  return 0;
+}
+
+int generate_roster_files(long int capacity, char *argv[]) {
+  int res = mkdir(ROSTERS_DIR, 0755);
+  if (res && errno != EEXIST) {
+    printf("res = %d\n", res);
+    printf("Failed to create directory %s: %s\n", ROSTERS_DIR, strerror(errno));
+    return errno;
+  }
+
+  printf("Generating rosters in %s... ", ROSTERS_DIR);
+  fflush(stdout);
+  long int i, j, next, prev;
+  FILE *fd;
+  ssize_t dir_len = strlen(ROSTERS_DIR);
+  char roster[BUFSIZE];
+  char user[BUFSIZE];
+  char file[BUFSIZE];
+  mk_roster_dat(roster, argv);
+  replace(user, argv[4], '%', "%lu");
+  memset(file, 0, BUFSIZE);
+  strcpy(user+strlen(user), ".dat");
+  strcpy(file, ROSTERS_DIR);
+  for (i=1; i<=capacity; i++) {
+    sprintf(file+dir_len, user, i);
+    fd = fopen(file, "w");
+    if (!fd) {
+      printf("Failed to open file %s for writing: %s\n", file, strerror(errno));
+      return errno;
+    }
+    if (fprintf(fd, "return {\n\t[\"pending\"] = {};\n") < 0) {
+      printf("Failed to write to file %s: %s\n", file, strerror(errno));
+      return errno;
+    }
+    for (j=1; j<=10; j++) {
+      next = i + j;
+      next = (next > capacity) ? (next % capacity): next;
+      prev = (i <= j) ? capacity - (j - 1) : i-j;
+      if ((fprintf(fd, roster, next, next) < 0) ||
+	  (fprintf(fd, roster, prev, prev) < 0)) {
 	printf("Failed to write to file %s: %s\n", file, strerror(errno));
 	return errno;
       }
-      fclose(fd);
     }
-    printf("done\n");
-    return 0;
-  } else {
-    printf("Failed to generate content for user*.dat: %s\n", strerror(ENOMEM));
-    return ENOMEM;
+    if (fprintf(fd, "};\n") < 0) {
+      printf("Failed to write to file %s: %s\n", file, strerror(errno));
+      return errno;
+    }
   }
+  printf("done\n");
+  return 0;
 }
 
-void print_hint(file_type t) {
+void print_hint(file_type t, char *argv[]) {
   if (t == T_CSV) {
     char *mysql_cmd =
       " LOAD DATA LOCAL INFILE '%s'\n"
@@ -184,6 +224,18 @@ void print_hint(file_type t) {
     printf("** PostgreSQL:\n");
     printf(pgsql_cmd, "users", USERS_CSV_FILE);
     printf(pgsql_cmd, "rosterusers", ROSTERS_CSV_FILE);
+  } else {
+    char server[BUFSIZE];
+    replace(server, argv[5], '.', "%2e");
+    printf("server = %s\n", server);
+    printf("Now copy %s and %s into Prosody spool directory. Something like:\n"
+	   " sudo rm -rf /var/lib/prosody/%s/accounts\n"
+	   " sudo rm -rf /var/lib/prosody/%s/roster\n"
+	   " sudo mv %s /var/lib/prosody/%s/\n"
+	   " sudo mv %s /var/lib/prosody/%s/\n"
+	   " sudo chown prosody:prosody -R /var/lib/prosody/%s\n",
+	   USERS_DIR, ROSTERS_DIR, server, server,
+	   USERS_DIR, server, ROSTERS_DIR, server, server);
   }
 }
 
@@ -201,7 +253,7 @@ int main(int argc, char *argv[]) {
 	if (!res) {
 	  res = generate_rosters_csv(capacity, argv);
 	  if (!res) {
-	    print_hint(T_CSV);
+	    print_hint(T_CSV, argv);
 	  }
 	}
       } else {
@@ -211,6 +263,12 @@ int main(int argc, char *argv[]) {
     } else if (!strcmp(argv[1], "prosody")) {
       if (!strcmp(argv[2], "flat")) {
 	res = generate_user_files(capacity, argv);
+	if (!res) {
+	  res = generate_roster_files(capacity, argv);
+	  if (!res) {
+	    print_hint(T_FLAT, argv);
+	  }
+	}
       } else {
 	printf("Usupported Prosody database type: %s\n", argv[2]);
 	res = -1;
